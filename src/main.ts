47cbe112be109eede5be1a1f4d66b038e93cc240
tourist <17322619133@163.com>
2022-12-23 11:15:50 +0800

    feat: use import.meta.glob handle vue-router children

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,12 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 import App from './App.vue'
+import router from './router'
 
 const app = createApp(App)
 
-import { createRouter, createWebHashHistory } from "vue-router";
-import r from "@/router";
-
 app
 .use(ElementPlus)
-.use(createRouter({history: createWebHashHistory(), routes: r.children}))
+.use(router)
+
 app.mount('#app')
