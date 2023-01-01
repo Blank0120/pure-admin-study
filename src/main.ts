79ee494e8c03be6789efb3cd33a22404e27e1355
tourist <17322619133@163.com>
2023-01-01 22:17:23 +0800

    fix: fix tailwind preflight and introduce pinia

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import 'element-plus/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 
 // 一定要在main.ts中导入tailwind.css，防止vite每次hmr都会请求src/style/index.scss整体css文件导致热更新慢的问题
 import './style/tailwind.css';
@@ -14,5 +15,6 @@ const app = createApp(App)
 app
 .use(ElementPlus)
 .use(router)
+.use(createPinia())
 
 app.mount('#app')
