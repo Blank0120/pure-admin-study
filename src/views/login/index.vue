53a43f996f35fe31230aacf38e52cca5ea338089
tourist <17322619133@163.com>
2023-01-01 21:31:51 +0800

    chore: rm unused code

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,15 +4,6 @@ import { onBeforeUnmount, onMounted, reactive, ref } from 'vue';
 
 import { loginRules } from "./utils/rule";
 
-import {
-  Check,
-  Delete,
-  Edit,
-  Message,
-  Search,
-  Star,
-} from '@element-plus/icons-vue'
-
 const loading = ref(false);
 const ruleFormRef = ref<FormInstance>();
 
@@ -76,15 +67,6 @@ onBeforeUnmount(() => {
 
       </el-form>
     </div>
-
-    <el-row class="mb-4">
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
-  </el-row>
 </template>
 
 <style scoped>
